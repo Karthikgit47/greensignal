1eaf405676ff5ea7adacd605ebc295a3c546592d
karthigit47 <karthism47@gmail.com>
2026-02-17 16:18:54 -1200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,22 @@
-// const CACHE_NAME = "greensignal-cache-v1";
-// const BASE_PATH = "/greensignal";
-
-// self.addEventListener("install", (event) => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => {
-//       return cache.addAll([
-//         `${BASE_PATH}/`,
-//         `${BASE_PATH}/index.html`
-//       ]);
-//     })
-//   );
-// });
-
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((response) => {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
-
 const CACHE_NAME = "greensignal-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json"
-];
+const BASE_PATH = "/greensignal";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        `${BASE_PATH}/`,
+        `${BASE_PATH}/index.html`
+      ]);
+    })
   );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", () => {
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(
-      (response) => response || fetch(event.request)
-    )
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
+
